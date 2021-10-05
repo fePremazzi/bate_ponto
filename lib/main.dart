5c49bd39a26b8b406e4a3546072285887ae44f43
fePremazzi <felliperego@hotmail.com>
2021-10-05 20:47:15 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HitDotApp());
+}
+
+class HitDotApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Bate ponto'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 32, 32, 8),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: "CPF",
+                hintText: 'Digite seu CPF',
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Bota o CPF, tenho cara de Bidu por um acaso?!?!';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Digite sua senha',
+                labelText: "Senha",
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Bota a senha, tenho cara de Bidu por um acaso?!?';
+                }
+                return null;
+              },
+              obscureText: true,
+            ),
+            MyStatefulCheckbox(),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Enviar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulCheckbox extends StatefulWidget {
+  const MyStatefulCheckbox({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulCheckbox> createState() => _CheckBoxStfull();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _CheckBoxStfull extends State<MyStatefulCheckbox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      return Colors.blueAccent;
+    }
+
+    return CheckboxListTile(
+      title: Text("Salvar cpf e senha"),
+      controlAffinity: ListTileControlAffinity.leading,
+      checkColor: Colors.white,
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
